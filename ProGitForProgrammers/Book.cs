e7ae8b8e718413875e2465cfa51fc5d7bf469d2e
Hiparco Martinez <hevenor81@gmail.com>
2022-09-08 15:24:52 -0600

    Add class Book

---
+++ ProGitForProgrammers/Book.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProGitForProgrammers
+{
+    public class Book
+    {
+        public String Title { get; set; }
+        public String Description { get; set; }
+
+
+    }
+}
